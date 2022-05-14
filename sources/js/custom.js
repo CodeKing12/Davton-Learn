c64bcafc4d71036b5f73306552c015690a653ea6
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-05-14 14:08:26 +0000

    I have done some things.

--- sources/js/custom.js
+++ sources/js/custom.js
@@ -7,27 +7,40 @@ $(".course-slider-1").slick({
     nextArrow: "<button class='flex absolute z-20 top-0 bottom-0 right-0 h-full w-12 px-1 items-center justify-center py-[6px] next-arrow text-white transition-all duration-100 ease-linear hover:duration-150 group'><span class='material-symbols-outlined !text-3xl group-hover:!text-4xl transition-all duration-150 ease hover:duration-150 !hidden'>arrow_forward_ios</span></button>",
 })
 
-$(".course-slider-2").slick({
-    variableWidth: true,
-    infinite: false,
-    prevArrow: "<button class='flex absolute z-20 top-0 bottom-0 left-0 h-full w-12 px-1 items-center justify-center py-[6px] next-arrow text-white transition-all duration-100 ease-linear hover:duration-150 group'><span class='material-symbols-outlined !text-3xl group-hover:!text-4xl transition-all duration-150 ease hover:duration-150 !hidden'>arrow_back_ios</span></button>",
-    nextArrow: "<button class='flex absolute z-20 top-0 bottom-0 right-0 h-full w-12 px-1 items-center justify-center py-[6px] next-arrow text-white transition-all duration-100 ease-linear hover:duration-150 group'><span class='material-symbols-outlined !text-3xl group-hover:!text-4xl transition-all duration-150 ease hover:duration-150 !hidden'>arrow_forward_ios</span></button>",
-})
 
 function openCard (element) {
     card = element.children[1]
     element.classList.add("pop-out")
     card.classList.add("show-me")
     imageCard = element.children[0]
-    imageCard.children[1].classList.remove("h-32")
-    imageCard.children[1].classList.add("h-[186px]")
+    // imageCard.children[1].classList.remove("h-36")
+    // imageCard.children[1].classList.add("h-[186px]")
 }
 
 function closeCard (element) {
     card = element.children[1]
     element.classList.remove("pop-out")
     card.classList.remove("show-me")
+    // imageCard.children[1].classList.remove("h-[186px]")
+    // imageCard.children[1].classList.add("h-36")
 } 
+
+function openMenu() {
+    document.getElementById("menu").style.width = "250px";
+    nav.classList.add("nav-active");
+    setTimeout(function(){document.querySelector(".mobile-nav-items").classList.remove("disappear");}, 400);
+    document.querySelector(".menu-icon").setAttribute("onclick", "closeMenu()");
+    document.querySelector("body").style.overflow = "hidden";
+}
+
+function closeMenu() {
+    setTimeout(function(){document.getElementById("menu").style.width = "0";}, 100);
+    // document.getElementById("menu").style.width = "0";
+    document.querySelector(".mobile-nav-items").classList.add("disappear");
+    nav.classList.remove("nav-active");
+    document.querySelector(".menu-icon").setAttribute("onclick", "openMenu()");
+    document.querySelector("body").style.overflow = "scroll";
+}
 
 // course_sliders = document.querySelectorAll(".all-course-sliders")[0]
 
