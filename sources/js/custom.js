dad8e222288e059533f45082e569e484f8beff87
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-06-09 19:29:11 +0100

    I have added some components to for the new groups and also added the news and modified sections.

--- sources/js/custom.js
+++ sources/js/custom.js
@@ -57,24 +57,14 @@ function showDropdown() {
     $('.profileDropdown').toggleClass("show-us")
 }
 
-// course_sliders = document.querySelectorAll(".all-course-sliders")[0]
 
-// course_sliders.forEach(el => el.onmouseover = function(){
-//     el.children.forEach(child => function() {
-//         if (child.classList.contains("slick-arrow")) {
-//             el.children[0].style.display="flex"
-//             el.children[0].style.backgroundColor="#000000"
-//             el.children[2].style.display="flex"
-//             el.children[2].style.backgroundColor="#000000"
-//         }
-//     })
-// })
+MicroModal.init();
 
-// for (slider in course_sliders) {
-//     course_sliders[slider].addEventListener("onmouseenter", function(){
-//         slider.style.display = "flex"
-//     });
-//     course_sliders[slider].addEventListener("onmouseleave", function(){
-//         slider.style.display = "none"
-//     })
-// }
+$(".news-slider").slick({
+    variableWidth: true,
+    infinite: false,
+    edgeFriction: 1,
+    draggable: false,
+    prevArrow: "<button class='flex absolute z-[200] top-0 bottom-0 left-0 h-full w-12 px-1 items-center justify-center py-[6px] next-arrow text-white transition-all duration-100 ease-linear hover:duration-150 group'><span class='material-symbols-outlined !text-3xl group-hover:!text-4xl transition-all duration-150 ease hover:duration-150 !hidden'>arrow_back_ios</span></button>",
+    nextArrow: "<button class='flex absolute z-[100] top-0 bottom-0 right-0 h-full w-12 px-1 items-center justify-center py-[6px] next-arrow text-white transition-all duration-100 ease-linear hover:duration-150 group'><span class='material-symbols-outlined !text-3xl group-hover:!text-4xl transition-all duration-150 ease hover:duration-150 !hidden'>arrow_forward_ios</span></button>",
+})
