6b16a317fa3d5c5878f8aa265242c2a8d1aa126e
Eyetu Kingsley <eyetukingsley330@gmail.com>
2023-07-13 02:47:17 +0100

    I added the manage page and made changes to fit it into the current tailwind configuration (and fix a lot of tailwind errors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,16 @@ module.exports = {
         poppins: ['Poppins', 'sans-serif'],
         ibmPlex: ['IBM Plex Sans', 'sans-serif']
       },
-      'colors': {
+      colors: {
         'brand-pink': '#D13A9C',
-        'brand-yellow': '#FFED4F'
+        'brand-yellow': '#FFED4F',
+        'brand-green': '#20A354',
+        // Imported
+        'grey-1': '#979797',
+        'dash-blue': '#F5FFF9',
+        'mild-black': '#484747',
+        'brand-black': '#222222',
+        'black-2': '#4F4F4F',
       },
       screens: {
         'xs': '350px',
@@ -17,7 +24,22 @@ module.exports = {
         'lg': '992px',
         'xl': '1200px',
         'xxl': '1400px',
-      }
+      },
+      // Imported
+      spacing: {
+        7.5: '30px'
+      },
+      borderRadius: {
+        5: '5px',
+        10: '10px'
+      },
+      zIndex: {
+        '60': '60'
+      },
+      fontSize: {
+        13: '13px',
+        15: '15px'
+      },
     },
   },
   plugins: [],
